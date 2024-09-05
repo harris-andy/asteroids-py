63ad0c2c9e38262b031ac73d79b3c98da143f236
harris-andy <agharris@gmail.com>
2024-09-05 16:23:32 -0400

    4. added more shit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +16,7 @@ def main():
                 return
             
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
         # clock.tick(60)
